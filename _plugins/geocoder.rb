cf11c89b972aa38c287f3a7cef89f26221d1579a
116 <116@lab0.net>
2015-12-16 14:49:10 +0100

    Add country code in geocoding

--- _plugins/geocoder.rb
+++ _plugins/geocoder.rb
@@ -12,16 +12,31 @@ Jekyll::Hooks.register :posts, :pre_render do |post|
 
   if location and location['name'] and ( not location['lat'] or not location['lon'])
     puts location.inspect
-    loc = GEO_CACHE[location["name"]]
+    loc = GEO_CACHE[location['name']]
     unless loc
-      puts "geocoding... #{location["name"]}"
-      url="http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape location["name"]}&sensor=false"
+      puts "geocoding... #{location['name']}"
+
+      # get location from name
+      url="http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape location['name']}&sensor=false"
       result = JSON.parse(open(url).read)
       loc = result['results'][0]['geometry']['location'] rescue 'none'
-      GEO_CACHE[location["name"]] = loc
+
+      puts loc
+      # get the country from the location
+      url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=#{loc['lat']},#{loc['lng']}&sensor=false"
+      result = JSON.parse(open(url).read)
+      countries = result['results'][0]['address_components'].select{|x| x['types'].include? 'country'}
+      puts countries
+      unless countries.empty?
+        countrycode = countries[0]['short_name']
+        loc['countrycode'] = countrycode.downcase
+      end
+
+      GEO_CACHE[location['name']] = loc
       update_geo_cache!
     end
-    data.merge!(loc) if loc != 'none'
+    data['location'].merge!(loc) if loc != 'none'
+    puts data
   end
 end
 
